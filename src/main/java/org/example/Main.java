cbb0a896b24a9f8e68a179fe90d92091924f8a93
natalliaSkiba <nt1711182@gmail.com>
2022-12-26 20:04:37 +0100

    stream1 -1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
-        Integer[] arr = {1, 2, 3, 4,5,6,7,8,9,10};
+        Integer[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int res = Arrays.stream(arr)
                 .map(el -> {
                     if (el % 2 == 0) {
